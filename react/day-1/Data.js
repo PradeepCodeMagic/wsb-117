ce14b8d02b9a5e21309199e93dbf5a2d301f2a9f
PradeepCodeMagic <nicklovin32@gmail.com>
2024-08-30 11:52:54 +0530

    updated

---
+++ react/day-1/Data.js
@@ -0,0 +1,24 @@
+ export let Data=[
+    {
+        id:1,
+        name:"ws"
+    },
+    {
+        id:2,
+        name:"pradeep"
+    },
+    {
+        id:3,
+        name:"jodhpur"
+    },
+    {
+        id:4,
+        name:"jaipur"
+    },
+    {
+        id:5,
+        name:"udaipur"
+    },
+]
+
+// module.exports={Data};
---
+++ react/day-1/index.js
@@ -0,0 +1,9 @@
+// const Data=require("./Data")
+
+
+// console.log(Data)
+
+
+import {Data} from "./Data.js"
+
+console.log(Data)
